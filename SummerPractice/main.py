096bf5155850e9ee3f84857b4dfd1795a21e16fd
anyal <anyalad5@gmail.com>
2025-07-25 13:27:24 +0300

    Доработка кода. Вынесены отдельные функции отрисовки графиков и печать результата. Добавлено описание алгоритма в файл README.md.

--- SummerPractice/main.py
+++ SummerPractice/main.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 from collections import defaultdict
 import matplotlib.pyplot as plt
 from datetime import datetime
@@ -11,6 +12,12 @@ class TrafficLightAnalyzer:
         self.data_main = defaultdict(list)
         self.data_left = defaultdict(list)
         self.data_right = defaultdict(list)
+        self.min_time = float('inf')
+        self.max_time = float('-inf')
+        self.times = []
+        self.main_colors = []
+        self.left_colors = []
+        self.right_colors = []
 
         self.color_priority = {
             '0': 0, 'unknown': 0,
@@ -39,36 +46,50 @@ class TrafficLightAnalyzer:
             9: 'flashing'
         }
 
+        self.flashing_priority = {
+            1: 3,  # Красный - высший приоритет
+            3: 2,  # Желтый
+            2: 1,  # Зеленый
+            4: 1,  # Зел. стрелка влево
+            5: 1,  # Зел. стрелка вправо
+            6: 1  # Зел. стрелка прямо
+        }
+
     def load_data(self, filename):
         with open(filename, 'r', encoding='utf-8') as file:
             reader = csv.reader(file)
-            for _ in range(3):
-                next(reader)
 
             for row in reader:
                 try:
-                    unix_time = float(row[0])
+                    try:
+                        unix_time = float(row[0])
+                    except ValueError as e:
+                        # print(f"Пропускаем строку: {row}.")
+                        continue
+
+                    self.min_time = min(unix_time, self.min_time)
+                    self.max_time = max(unix_time, self.max_time)
                     camera = row[2]
 
                     # Основной сигнал
                     main_color = self.color_priority.get(str(row[3]).lower(), 0)
                     main_status = int(row[4]) if len(row) > 4 and row[4] else 8
                     main_conf = float(row[5])
-                    self.data_main[camera].append((unix_time, main_color,main_status, main_conf))
+                    self.data_main[camera].append((unix_time, main_color, main_status, main_conf))
 
                     # Левая секция
                     left_color = self.color_priority.get(str(row[11]).lower(), 0) if len(row) > 11 and row[11] else 0
                     left_status = int(row[12]) if len(row) > 12 and row[12] else 8
                     left_conf = float(row[13]) if len(row) > 13 and row[13] else 0
                     if left_color != 0 or left_conf != 0:  # Только если есть данные
-                        self.data_left[camera].append((unix_time, left_color,left_status, left_conf))
+                        self.data_left[camera].append((unix_time, left_color, left_status, left_conf))
 
                     # Правая секция
                     right_color = self.color_priority.get(str(row[7]).lower(), 0) if len(row) > 7 and row[7] else 0
                     right_status = int(row[8]) if len(row) > 8 and row[8] else 8
                     right_conf = float(row[9]) if len(row) > 9 and row[9] else 0
                     if right_color != 0 or right_conf != 0:  # Только если есть данные
-                        self.data_right[camera].append((unix_time, right_color,right_status, right_conf))
+                        self.data_right[camera].append((unix_time, right_color, right_status, right_conf))
 
                 except (IndexError, ValueError) as e:
                     print(f"Ошибка обработки строки: {row}. Ошибка: {e}")
@@ -80,7 +101,7 @@ class TrafficLightAnalyzer:
 
         for camera, observations in data_source.items():
             for obs in observations:
-                time, color, status,  confidence = obs
+                time, color, status, confidence = obs
                 if abs(time - t) <= self.delta_t / 2 and confidence >= self.confidence_threshold:
                     if status == 9:
                         flashing_colors.add(color)
@@ -92,16 +113,8 @@ class TrafficLightAnalyzer:
         # Приоритет для мигающих сигналов
         if flashing_colors:
             # Выбираем самый приоритетный мигающий сигнал
-            flashing_priority = {
-                1: 3,  # Красный - высший приоритет
-                3: 2,  # Желтый
-                2: 1,  # Зеленый
-                4: 1,  # Стрелки
-                5: 1,
-                6: 1
-            }
             # Находим мигающий цвет с максимальным приоритетом
-            flashing_color = max(flashing_colors, key=lambda x: flashing_priority.get(x, 0))
+            flashing_color = max(flashing_colors, key=lambda x: self.flashing_priority.get(x, 0))
             return flashing_color
 
         # Обычная обработка для SOLID сигналов
@@ -122,10 +135,9 @@ class TrafficLightAnalyzer:
 
         # Отображение исходных точек
         for camera, observations in color_map['data'].items():
-            cam_times = [obs[0] for obs in observations] # Извлекаем временные метки
-            cam_colors = [obs[1] for obs in observations] # Извлекаем цвета
+            cam_times = [obs[0] for obs in observations]  # Извлекаем временные метки
+            cam_colors = [obs[1] for obs in observations]  # Извлекаем цвета
             plt.scatter(cam_times, cam_colors, alpha=0.3, label=f'Камера {camera} (raw)', s=30)
-
 
         # Сглаженная линия
         plt.plot(times, colors, 'k-', linewidth=2, label='Сглаженный сигнал')
@@ -137,7 +149,7 @@ class TrafficLightAnalyzer:
         plt.ylabel(ylabel)
         plt.title(title)
 
-        #plt.legend()
+        # plt.legend()
 
         # Убираем дубликаты в легенде
         handles, labels = plt.gca().get_legend_handles_labels()
@@ -148,29 +160,33 @@ class TrafficLightAnalyzer:
         plt.tight_layout()
         plt.show()
 
-    def analyze_time_range(self, start_unix, end_unix, step_sec=1.0):
-        times = []
-        main_colors = []
-        left_colors = []
-        right_colors = []
+    def analyze_time_range(self, step_sec=1.0):
+        self.times = []
+        self.main_colors = []
+        self.left_colors = []
+        self.right_colors = []
+        start_unix = self.min_time
+        end_unix = self.max_time
 
         current = start_unix
         while current <= end_unix:
-            times.append(current)
-            main_colors.append(self.get_color_at_time(current, self.data_main))
-            left_colors.append(self.get_color_at_time(current, self.data_left))
-            right_colors.append(self.get_color_at_time(current, self.data_right))
+            self.times.append(current)
+            self.main_colors.append(self.get_color_at_time(current, self.data_main))
+            self.left_colors.append(self.get_color_at_time(current, self.data_left))
+            self.right_colors.append(self.get_color_at_time(current, self.data_right))
             current += step_sec
 
+    def print_result(self):
         # Вывод результатов
         print("\nУсредненная последовательность:")
         print("Время         | Основной | Левая | Правая")
         print("------------------------------------------")
         # вывод в числах
-        for t, m, l, r in zip(times, main_colors, left_colors, right_colors):
+        for t, m, l, r in zip(self.times, self.main_colors, self.left_colors, self.right_colors):
             print(f"{t:.1f} | {m:>8} | {l:>5} | {r:>5}")
-            #print(f"{t:.1f} | {self.color_names[m]:<8} | {self.color_names[l]:<5} | {self.color_names[r]}") вывод словами
+            # print(f"{t:.1f} | {self.color_names[m]:<8} | {self.color_names[l]:<5} | {self.color_names[r]}") вывод словами
 
+    def plot_graphs(self):
         # Настройки графиков для каждой секции
         main_settings = {
             'data': self.data_main,
@@ -188,23 +204,44 @@ class TrafficLightAnalyzer:
 
         right_settings = {
             'data': self.data_right,
-            'ticks': {0: 'Не опр.', 1: 'Красный',2: 'Зеленый', 5: 'Зел.стрелка вправо', 6: 'Вперед', 7: 'Назад'},
+            'ticks': {0: 'Не опр.', 1: 'Красный', 2: 'Зеленый', 5: 'Зел.стрелка вправо', 6: 'Вперед', 7: 'Назад'},
             'title': 'Правая секция светофора',
             'ylabel': 'Состояние'
         }
 
         # Построение отдельных графиков
-        self.plot_single_signal(times, main_colors, main_settings, main_settings['title'], main_settings['ylabel'])
-        self.plot_single_signal(times, left_colors, left_settings, left_settings['title'], left_settings['ylabel'])
-        self.plot_single_signal(times, right_colors, right_settings, right_settings['title'], right_settings['ylabel'])
+        self.plot_single_signal(self.times, self.main_colors, main_settings, main_settings['title'],
+                                main_settings['ylabel'])
+        self.plot_single_signal(self.times, self.left_colors, left_settings, left_settings['title'],
+                                left_settings['ylabel'])
+        self.plot_single_signal(self.times, self.right_colors, right_settings, right_settings['title'],
+                                right_settings['ylabel'])
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: main.py file.csv [--plot] [--noprint]")
+        print("file.csv: traffic light data")
+        print("Options:")
+        print("--plot: draw graphs")
+        print("--noprint: don't print results")
+        sys.exit()
     analyzer = TrafficLightAnalyzer(delta_t=1.0, confidence_threshold=70.0)
-    analyzer.load_data('classified_tls.csv')
+    fileName = sys.argv[1]
+    analyzer.load_data(fileName)
 
-    analyzer.analyze_time_range(
-        start_unix=1720612825.0,
-        end_unix=1720612940.0,
-        step_sec=0.5
-    )
+    # print(analyzer.min_time, analyzer.max_time)
+
+    analyzer.analyze_time_range(step_sec=0.5)
+
+    try:
+        sys.argv.index('--noprint')
+    except ValueError as e:
+        analyzer.print_result()
+
+    try:
+        sys.argv.index('--plot')
+        analyzer.plot_graphs()
+    except ValueError as e:
+        # не рисуем график
+        a = 0
